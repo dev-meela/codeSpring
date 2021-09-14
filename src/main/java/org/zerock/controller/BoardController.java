f36d4d50ce4fe8196a22d55cb29d2cd4406236fe
ecficio <hyunjooy7@gmail.com>
2021-09-14 14:30:00 +0900

    27. 게시물 삭제와 첨부파일

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -1,5 +1,8 @@
 package org.zerock.controller;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -30,6 +33,34 @@ import lombok.extern.log4j.Log4j;
 public class BoardController {
 	
 	private BoardService service;
+	
+	private void deleteFiles(List<BoardAttachVO> attachList) {
+		
+		if (attachList == null || attachList.size() == 0) {
+			return;
+		}
+		
+		log.info("delete attach files ==== ");
+		log.info(attachList);
+		
+		attachList.forEach(attach -> {
+			try {
+				Path file = Paths.get("C:\\upload\\" + attach.getUploadPath() + "\\" + attach.getUuid() + "_" + attach.getFileName());
+				
+				Files.deleteIfExists(file);
+				
+				if(Files.probeContentType(file).startsWith("image")) {
+					
+					Path thumbNail = Paths.get("C:\\upload\\" + attach.getUploadPath() + "\\s_" + attach.getUuid() + "_" + attach.getFileName());
+					
+					Files.delete(thumbNail);
+				}
+			} catch(Exception e) {
+				log.error("delete file error" + e.getMessage());
+			} // end catch
+		}); // end foreach
+	}
+	
 	/*
 	@GetMapping("/list")
 	public void list(Model model) {
@@ -102,7 +133,13 @@ public class BoardController {
 	public String remove(@RequestParam("bno") Long bno, Criteria cri, RedirectAttributes rttr) {
 		
 		log.info("remove " + bno);
+		
+		List<BoardAttachVO> attachList = service.getAttachList(bno);
+		
 		if (service.remove(bno)) {
+			
+			deleteFiles(attachList);
+			
 			rttr.addFlashAttribute("result", "success");
 		}
 		/*
--- src/main/java/org/zerock/mapper/BoardAttachMapper.java
+++ src/main/java/org/zerock/mapper/BoardAttachMapper.java
@@ -11,4 +11,6 @@ public interface BoardAttachMapper {
 	public void delete(String uuid);
 	
 	public List<BoardAttachVO> findByBno(Long bno);  // 게시물 번호로 첨부파일을 찾음
+	
+	public void deleteAll(Long bno);
 }
